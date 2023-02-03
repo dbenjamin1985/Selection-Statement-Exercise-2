c95c76d44de1df9bcaa6d36f18b9b9de93b3c83d
David Benjamin <davidbenjamin0685@gmail.com>
2023-02-03 13:28:09 -0700

    Exercise 2

--- SelectionStatementsExercise2/Program.cs
+++ SelectionStatementsExercise2/Program.cs
@@ -4,7 +4,35 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
+            Console.WriteLine("What is your favorite school subject?");
+            string subject = (Console.ReadLine());
+            
+            switch (subject)
+            {
+                case "math":
+                    Console.WriteLine("Not bad! Math is decent!");
+                    break;
+                case "science":
+                    Console.WriteLine("Yikes! You must be smart!");
+                    break;
+                case "history":
+                    Console.WriteLine("I see you like to learn about the past! Good stuff!");
+                    break;
+                case "english":
+                    Console.WriteLine("Yuck! Go write an essay on how you can be better than that! C'mon!");
+                    break;
+                case "music":
+                    Console.WriteLine("Nice! Music is one of my favs!");
+                    break;
+                case "art":
+                    Console.WriteLine("Okay Picasso, calm down!");
+                    break;
+                default:
+                    Console.WriteLine("Yawn! Pick something cooler!!");
+                    break;
+
+            }
         }
     }
 }
